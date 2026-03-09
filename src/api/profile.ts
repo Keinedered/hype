c8d6bfe71bcf937e72e32d6a3194e1d78535b8fb
safonovoplatit <sazonovpeters@gmail.com>
2026-03-09 15:21:13 +0300

    fix profile

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -2,6 +2,7 @@ import { axiosClient } from './axiosClient';
 import { UserProfile } from '../types/user-profile';
 
 export async function getMyProfile(): Promise<UserProfile> {
-  const response = await axiosClient.get<UserProfile>('/user/me');
+  const response = await axiosClient.get<UserProfile>('/users/me');
   return response.data;
 }
+
